f9f5c2aafe7f39a84a63b74cde52af74f64023e0
Paolinni <pxv2009@gmail.com>
2016-01-25 22:56:49 -0600

    api content rendering working

--- src/index.js
+++ src/index.js
@@ -1,23 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 
+import VideoList from './components/video_list'
 import SearchBar from './components/search_bar'
 import API_KEY from './components/keys'
 
-console.log('API_KEY: ', API_KEY);
+class App extends Component {
+  constructor(props) {
+    super(props);
 
-YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
-  console.log(data);
-});
-// Create a new component. This component should produce some HTML
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos}/>
+      </div>
+    )
+  }
 }
 
-// Take this component's generated HTML and put it on the page (in the DOM)
 ReactDOM.render(<App />, document.querySelector('.container'));
